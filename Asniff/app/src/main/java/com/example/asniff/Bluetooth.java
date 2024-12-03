fcc53af89702079ec484d70651641651e66bd94e
Pablo  Aguilar <135460549+pablo-972@users.noreply.github.com>
2024-12-03 20:08:42 +0100

    comienzo

---
+++ Asniff/app/src/main/java/com/example/asniff/Bluetooth.java
@@ -0,0 +1,95 @@
+package com.example.asniff;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Bluetooth extends AppCompatActivity {
+
+    private ListView listaDispositivos;
+    private ArrayAdapter<String> listaAdaptadaDispositivos;
+    private BluetoothAdapter bluetoothAdapter;
+    private Toast toast;
+
+    private static final int PERMISSION_REQUEST_CODE = 100;
+
+    @RequiresApi(api = Build.VERSION_CODES.S)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_bluetooth);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{
+                    android.Manifest.permission.BLUETOOTH_SCAN,
+            }, PERMISSION_REQUEST_CODE);
+        }
+
+        //Inicializamos
+        listaDispositivos = findViewById(R.id.dispositivosBluetooth);
+        listaAdaptadaDispositivos = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        listaDispositivos.setAdapter(listaAdaptadaDispositivos);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(bluetoothAdapter == null){
+            toast = Toast.makeText(getApplicationContext(), "ERROR AL DETECTAR EL ADAPTADOR BLUETOOTH", Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+        if(!bluetoothAdapter.isEnabled()){
+            toast = Toast.makeText(getApplicationContext(), "ACTIVE EL SERVICIO BLUETOOTH", Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
+
+
+        bluetoothAdapter.startDiscovery();
+    }
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(BluetoothDevice.ACTION_FOUND.equals(action)){
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String macAddress = device.getAddress();
+
+                if(deviceName != null){
+                    String info = "Nombre: " + deviceName + "\n Dirección MAC: " + macAddress;
+                    listaAdaptadaDispositivos.add(info);
+                    listaAdaptadaDispositivos.notifyDataSetChanged();
+                }
+            }
+        }
+    };
+
+}
